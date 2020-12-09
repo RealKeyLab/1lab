4b28f941f5e58205cd5cb37238f7a41ed8f51861
MachekhinKV@studklg.ru <olimp@university.kspu.kaluga.ru>
2020-12-09 10:21:01 +0300

    machekhin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
+import Machehin from './containers/Machehin';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Machehin />
+      
     </div>
   );
 }
